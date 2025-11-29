b3b482413b49198f338f341beebb29483065efc9
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:00:50 -0400

    NavHost actualizado

--- app/src/main/java/com/autoparts/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/com/autoparts/presentation/navigation/AppNavHost.kt
@@ -4,35 +4,64 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.autoparts.presentation.Inicio.HomeScreen
 import com.autoparts.presentation.Login.LoginScreen
+import com.autoparts.presentation.productos.ProductosScreen
+import com.autoparts.presentation.productodetalle.ProductoDetalleScreen
+
 
 @Composable
 fun AppNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
-    androidx.navigation.NavHost(
+    NavHost(
         navController = navController,
-        startDestination = Screen.Login.route,
+        startDestination = "home_screen",
         modifier = modifier
-    ) {
+    ){
         composable(
             route = Screen.Home.route,
             arguments = listOf(
-                navArgument(Screen.Home.ARG) { type = NavType.IntType }
+                navArgument(Screen.Home.ARG){
+                    type = NavType.IntType
+                    defaultValue = -1
+                    nullable = false
+                }
             )
-        ) { backStackEntry ->
-            val id = backStackEntry.arguments?.getInt(Screen.Home.ARG)
+        ) {backStackEntry ->
+            val id = backStackEntry.arguments?.getInt(Screen.Home.ARG)?.takeIf { it != -1 }
             HomeScreen(
                 navController,
-                usuarioId = id
+                userId = id as String?
+            )
+        }
+        composable("home_screen") {
+            HomeScreen(
+                navController,
+                userId = null
             )
         }
         composable(Screen.Login.route) {
             LoginScreen(navController)
+        }
+        composable(Screen.Productos.route) {
+            ProductosScreen(navController)
+        }
+        composable(
+            route = Screen.ProductoDetalle.route,
+            arguments = listOf(
+                navArgument(Screen.ProductoDetalle.ARG){type = NavType.IntType}
+            )
+        ) { backStackEntry ->
+            val productoId = backStackEntry.arguments?.getInt(Screen.ProductoDetalle.ARG) ?: 0
+            ProductoDetalleScreen(
+                navController = navController,
+                productoId = productoId
+            )
         }
     }
 
