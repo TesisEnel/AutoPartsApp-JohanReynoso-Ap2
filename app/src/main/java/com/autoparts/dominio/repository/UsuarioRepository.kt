0f470c1526833ad406ac737b717f27820acec162
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:33:21 -0400

    UsuarioRepository actualizado

--- app/src/main/java/com/autoparts/dominio/repository/UsuarioRepository.kt
+++ app/src/main/java/com/autoparts/dominio/repository/UsuarioRepository.kt
@@ -1,12 +1,26 @@
 package com.autoparts.dominio.repository
 
 import com.autoparts.Data.Remote.Resource
+import com.autoparts.dominio.model.CreateUser
+import com.autoparts.dominio.model.LoginUser
+import com.autoparts.dominio.model.UpdateUser
 import com.autoparts.dominio.model.Usuarios
 import kotlinx.coroutines.flow.Flow
 
 interface UsuarioRepository {
-    suspend fun getUsuarios(): Flow<List<Usuarios>>
-    suspend fun getUsuario(id: Int): Resource<Usuarios?>
-    suspend fun putUsuario(id: Int, usuario: Usuarios): Resource<Unit>
-    suspend fun postUsuario(usuario: Usuarios): Resource<Usuarios?>
+    suspend fun getUsers(): Flow<List<Usuarios>>
+
+    suspend fun getUser(id: String): Resource<Usuarios>
+
+    suspend fun register(createUser: CreateUser): Resource<Usuarios>
+
+    suspend fun login(loginUser: LoginUser): Resource<Usuarios>
+
+    suspend fun updateUser(id: String, updateUser: UpdateUser): Resource<Unit>
+
+    suspend fun deleteUser(id: String): Resource<Unit>
+
+    suspend fun getUserByEmail(email: String): Resource<Usuarios>
+
+    suspend fun test(): Resource<Map<String, Any>>
 }
