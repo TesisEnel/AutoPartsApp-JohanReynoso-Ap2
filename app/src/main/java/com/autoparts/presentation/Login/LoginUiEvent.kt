04afbbe7296ed94cb40e69adeff5def220b37dc9
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:31:22 -0400

    LoginUiEvent actualizado

--- app/src/main/java/com/autoparts/presentation/Login/LoginUiEvent.kt
+++ app/src/main/java/com/autoparts/presentation/Login/LoginUiEvent.kt
@@ -1,8 +1,9 @@
 package com.autoparts.presentation.Login
 
 interface LoginUiEvent {
-    data class userNameChanged(val userName: String) : LoginUiEvent
+    data class emailChanged(val email: String) : LoginUiEvent
     data class passwordChanged(val password: String) : LoginUiEvent
+    data class phoneNumberChanged(val phoneNumber: String) : LoginUiEvent
     data object registerModeClicked: LoginUiEvent
     data object submitRegistration: LoginUiEvent
     data object submitLogin: LoginUiEvent
