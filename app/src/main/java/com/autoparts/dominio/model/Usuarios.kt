41f6ab8f7bd893c2ffc85d4fe848b859ef64bf3d
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:26:06 -0400

    Usuarios modificados

--- app/src/main/java/com/autoparts/dominio/model/Usuarios.kt
+++ app/src/main/java/com/autoparts/dominio/model/Usuarios.kt
@@ -1,7 +1,30 @@
 package com.autoparts.dominio.model
 
 data class Usuarios(
-    val usuarioId: Int?,
-    val userName: String,
+    val id: String = "",
+    val userName: String? = null,
+    val email: String = "",
+    val phoneNumber: String? = null,
+    val emailConfirmed: Boolean = false,
+    val roles: List<String> = emptyList()
+)
+
+data class CreateUser(
+    val email: String,
+    val password: String,
+    val phoneNumber: String? = null
+)
+
+// Para login
+data class LoginUser(
+    val email: String,
     val password: String
 )
+
+data class UpdateUser(
+    val email: String? = null,
+    val phoneNumber: String? = null,
+    val currentPassword: String? = null,
+    val newPassword: String? = null
+)
+
