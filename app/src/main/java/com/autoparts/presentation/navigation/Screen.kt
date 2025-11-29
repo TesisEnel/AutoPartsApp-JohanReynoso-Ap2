50aa0ea90c909c042649c360886761f1404e6e20
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:34:04 -0400

    Screen actualizado

--- app/src/main/java/com/autoparts/presentation/navigation/Screen.kt
+++ app/src/main/java/com/autoparts/presentation/navigation/Screen.kt
@@ -1,9 +1,14 @@
 package com.autoparts.presentation.navigation
 
 sealed class Screen(val route: String){
-    data object Home: Screen("home_screen/{id}"){
+    data object Home: Screen("home_screen?id={id}"){
         const val ARG = "id"
-        fun createRoute(id: Int) = "home_screen/$id"
+        fun createRoute(id: String? = null) = if (id != null) "home_screen?id=$id" else "home_screen"
     }
     data object Login: Screen("login_screen")
+    data object Productos: Screen("productos_screen")
+    data object ProductoDetalle: Screen("producto_detalle/{productoId}"){
+        const val ARG = "productoId"
+        fun createRoute(productoId: Int) = "producto_detalle/$productoId"
+    }
 }
