86457e3dd7e9d7c7ff9ce8f1f6ada038d8ae07a1
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:34:40 -0400

    SaveUsuarioUseCase

--- app/src/main/java/com/autoparts/dominio/usecase/SaveUsuarioUseCase.kt
+++ app/src/main/java/com/autoparts/dominio/usecase/SaveUsuarioUseCase.kt
@@ -1,22 +1,26 @@
 package com.autoparts.dominio.usecase
 
 import com.autoparts.Data.Remote.Resource
-import com.autoparts.dominio.model.Usuarios
+import com.autoparts.dominio.model.UpdateUser
 import com.autoparts.dominio.repository.UsuarioRepository
 import javax.inject.Inject
 
-class SaveUsuarioUseCase @Inject constructor(
+class UpdateUsuarioUseCase @Inject constructor(
     private val repository: UsuarioRepository
 ) {
-    suspend operator fun invoke(id: Int, usuario: Usuarios): Resource<Usuarios?> {
-        return if(id == 0){
-            repository.postUsuario(usuario)
-        }else{
-            when(repository.putUsuario(id, usuario)){
-                is Resource.Success -> Resource.Success(usuario)
-                is Resource.Error -> Resource.Error("Error al actualizar el usuario")
-                else -> Resource.Error("Error desconocido")
-            }
-        }
+    suspend operator fun invoke(
+        id: String,
+        email: String? = null,
+        phoneNumber: String? = null,
+        currentPassword: String? = null,
+        newPassword: String? = null
+    ): Resource<Unit> {
+        val updateUser = UpdateUser(
+            email = email,
+            phoneNumber = phoneNumber,
+            currentPassword = currentPassword,
+            newPassword = newPassword
+        )
+        return repository.updateUser(id, updateUser)
     }
 }
