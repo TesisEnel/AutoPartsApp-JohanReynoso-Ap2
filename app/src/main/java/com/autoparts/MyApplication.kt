5eff484bd91db7407dff3d8b8b9b95615220d940
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:46:04 -0400

    MyApplication actualizado

--- app/src/main/java/com/autoparts/MyApplication.kt
+++ app/src/main/java/com/autoparts/MyApplication.kt
@@ -1,8 +1,33 @@
 package com.autoparts
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication: Application() {
+class MyApplication: Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .respectCacheHeaders(false)
+            .build()
+    }
 }
