243652b9779eb1439d659f83f4cec59ef94e4b06
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:29:04 -0400

    InicioUievent actualizado

--- app/src/main/java/com/autoparts/presentation/Inicio/InicioUiEvent.kt
+++ app/src/main/java/com/autoparts/presentation/Inicio/InicioUiEvent.kt
@@ -1,13 +1,15 @@
 package com.autoparts.presentation.Inicio
 
 interface InicioUiEvent {
-    data class LoadUser(val usuarioId: Int) : InicioUiEvent
-    data class UserNameChanged(val userName: String) : InicioUiEvent
-    data class PasswordChanged(val password: String) : InicioUiEvent
+    data class LoadUser(val userId: String) : InicioUiEvent
+    data class EmailChanged(val email: String) : InicioUiEvent
+    data class PhoneNumberChanged(val phoneNumber: String) : InicioUiEvent
+    data class SearchQueryChanged(val query: String) : InicioUiEvent
     data object Save : InicioUiEvent
     data object Logout : InicioUiEvent
     data object UserMessageShown : InicioUiEvent
     data object showDialogEdit: InicioUiEvent
     data object hideDialogEdit: InicioUiEvent
+    data object LoadProductos : InicioUiEvent
 }
 
