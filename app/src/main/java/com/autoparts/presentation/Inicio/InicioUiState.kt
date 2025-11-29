53c42bb16e03b15ef0ea5a183a88ecf70cbd999b
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:29:18 -0400

    InicioUiState actualizado

--- app/src/main/java/com/autoparts/presentation/Inicio/InicioUiState.kt
+++ app/src/main/java/com/autoparts/presentation/Inicio/InicioUiState.kt
@@ -1,16 +1,20 @@
 package com.autoparts.presentation.Inicio
 
 import com.autoparts.dominio.model.Usuarios
+import com.autoparts.dominio.model.Producto
 
 data class InicioUiState(
-    val usuarioId: Int? = null,
+    val userId: String? = null,
     val isLoadingUser: Boolean = false,
     val isLoadingList: Boolean = false,
+    val isLoadingProductos: Boolean = false,
     val listUsuarios: List<Usuarios> = emptyList(),
-    val userName: String = "",
-    val userNameError: String? = null,
-    val password: String = "",
-    val passwordError: String? = null,
+    val listProductos: List<Producto> = emptyList(),
+    val email: String = "",
+    val emailError: String? = null,
+    val phoneNumber: String = "",
+    val phoneNumberError: String? = null,
     val userMessage: String? = null,
-    val showDialog: Boolean = false
+    val showDialog: Boolean = false,
+    val searchQuery: String = ""
 )
