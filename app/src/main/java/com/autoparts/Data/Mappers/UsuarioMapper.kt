31aded0fa236eae232d4d877f41b433f36890342
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-30 14:52:20 -0400

    UsuarioMapper actualizado

--- app/src/main/java/com/autoparts/Data/Mappers/UsuarioMapper.kt
+++ app/src/main/java/com/autoparts/Data/Mappers/UsuarioMapper.kt
@@ -2,9 +2,11 @@ package com.autoparts.Data.Mappers
 
 import com.autoparts.Data.Remote.Dto.CreateUserDto
 import com.autoparts.Data.Remote.Dto.LoginDto
+import com.autoparts.Data.Remote.Dto.LoginResponse
 import com.autoparts.Data.Remote.Dto.UpdateUserDto
 import com.autoparts.Data.Remote.Dto.UserDto
 import com.autoparts.dominio.model.CreateUser
+import com.autoparts.dominio.model.LoginResult
 import com.autoparts.dominio.model.LoginUser
 import com.autoparts.dominio.model.UpdateUser
 import com.autoparts.dominio.model.Usuarios
@@ -41,6 +43,13 @@ fun CreateUser.toDto(): CreateUserDto = CreateUserDto(
 fun LoginUser.toDto(): LoginDto = LoginDto(
     email = email,
     password = password
+)
+
+fun LoginResponse.toDomain(email: String): LoginResult = LoginResult(
+    accessToken = accessToken,
+    refreshToken = refreshToken,
+    expiresIn = expiresIn,
+    email = email
 )
 
 fun UpdateUser.toDto(): UpdateUserDto = UpdateUserDto(
