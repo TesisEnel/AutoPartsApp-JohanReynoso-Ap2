0dda244741a38997f0a08e43d35b0799c2a44d48
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-30 14:55:59 -0400

    ProductoDetalleUiState actualizado

--- app/src/main/java/com/autoparts/presentation/productodetalle/ProductoDetalleUiState.kt
+++ app/src/main/java/com/autoparts/presentation/productodetalle/ProductoDetalleUiState.kt
@@ -3,8 +3,6 @@ package com.autoparts.presentation.productodetalle
 data class ProductoDetalleUiState(
     val productoId: Int? = null,
     val isLoading: Boolean = false,
-
-    // Campos principales
     val productoNombre: String = "",
     val productoNombreError: String? = null,
     val productoMonto: String = "",
@@ -13,16 +11,12 @@ data class ProductoDetalleUiState(
     val productoCantidadError: String? = null,
     val productoDescripcion: String = "",
     val productoDescripcionError: String? = null,
-
-    // Categoría
     val categoria: String = "",
     val categoriaError: String? = null,
-
-    // Imagen y fecha
     val productoImagenUrl: String = "",
     val fecha: String = "",
-
-    // Estado UI
     val isEditMode: Boolean = false,
-    val userMessage: String? = null
+    val userMessage: String? = null,
+    val cantidadParaCarrito: Int = 1,
+    val isAddingToCarrito: Boolean = false
 )
