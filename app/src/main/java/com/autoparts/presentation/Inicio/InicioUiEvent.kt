dd6ec4fec2aaefc433fe15d38eb7142020c03209
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-30 15:01:22 -0400

    InicioUiEvent actualizado

--- app/src/main/java/com/autoparts/presentation/Inicio/InicioUiEvent.kt
+++ app/src/main/java/com/autoparts/presentation/Inicio/InicioUiEvent.kt
@@ -5,6 +5,8 @@ interface InicioUiEvent {
     data class EmailChanged(val email: String) : InicioUiEvent
     data class PhoneNumberChanged(val phoneNumber: String) : InicioUiEvent
     data class SearchQueryChanged(val query: String) : InicioUiEvent
+    data class CategorySelected(val category: String?) : InicioUiEvent
+    data class AddToCarrito(val productoId: Int) : InicioUiEvent
     data object Save : InicioUiEvent
     data object Logout : InicioUiEvent
     data object UserMessageShown : InicioUiEvent
@@ -12,4 +14,3 @@ interface InicioUiEvent {
     data object hideDialogEdit: InicioUiEvent
     data object LoadProductos : InicioUiEvent
 }
-
