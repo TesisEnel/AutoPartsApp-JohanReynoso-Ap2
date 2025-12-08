f3f99d1606f1eadd601c9081a703d9fb0fac0487
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-12-07 21:55:38 -0400

    feat ✨, modelo Venta agregado

---
+++ app/src/main/java/com/autoparts/domain/model/Venta.kt
@@ -0,0 +1,34 @@
+package com.autoparts.domain.model
+
+data class Venta(
+    val ventaId: Int,
+    val applicationUserId: String,
+    val fecha: String,
+    val total: Double,
+    val detalles: List<VentaDetalle>,
+    val pago: PagoInfo
+)
+
+data class VentaDetalle(
+    val detalleId: Int,
+    val productoId: Int,
+    val productoNombre: String,
+    val cantidad: Double,
+    val precioUnitario: Double,
+    val subtotal: Double
+)
+
+data class PagoInfo(
+    val pagoId: Int,
+    val nombreTitular: String,
+    val numeroTarjetaEnmascarado: String,
+    val direccion: String
+)
+
+data class CreateVenta(
+    val nombreTitular: String,
+    val numeroTarjeta: String,
+    val fechaExpiracion: String,
+    val cvv: String,
+    val direccion: String
+)
