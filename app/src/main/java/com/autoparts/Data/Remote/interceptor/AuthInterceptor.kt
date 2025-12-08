866ff3e9c03b3c60acfaad26ae7ce1d36e453cb9
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-12-07 21:47:27 -0400

    feat ✨, Interceptor de okhttp agregado

---
+++ app/src/main/java/com/autoparts/Data/Remote/interceptor/AuthInterceptor.kt
@@ -0,0 +1,28 @@
+package com.autoparts.data.remote.interceptor
+
+import com.autoparts.data.local.manager.SessionManager
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val sessionManager: SessionManager
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = runBlocking {
+            sessionManager.getToken().first()
+        }
+
+        val request = if (!token.isNullOrEmpty()) {
+            chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+        } else {
+            chain.request()
+        }
+
+        return chain.proceed(request)
+    }
+}
